e66c825435a4b241f901c1ad48624a7212cd1b8e
JuanRendon <juanrendon1996@hotmail.com>
2022-12-10 18:24:05 -0500

    Development project start

---
+++ src/main/java/com/sophos/jfrgBank/JfrgBankApplication.java
@@ -0,0 +1,13 @@
+package com.sophos.jfrgBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JfrgBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JfrgBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sophos/jfrgBank/JfrgBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sophos.jfrgBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JfrgBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
